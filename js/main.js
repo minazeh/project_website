054b511695fa687c76102f488d433e4e71ef5e66
minazeh <conradng07@gmail.com>
2020-12-27 15:23:55 +0800

    added the layout for the system

--- js/main.js
+++ js/main.js
@@ -1,28 +1,49 @@
-(function($){
+(function ($) {
 
-    const logo = new Freezeframe('.freezeGIF', {
-        trigger: 'click'        
-     });
-
-    logo.on('start', function(){
-
-        alert('something is not right?');
-
+    const card_1 = new Freezeframe('.freezeGIF_1', {
+        trigger: 'false'
     });
-      
-
-    $(document).ready(function(){
-
-        logo.stop(); // stop animation
-
+    const card_2 = new Freezeframe('.freezeGIF_2', {
+        trigger: 'false'
+    });
+    const card_3 = new Freezeframe('.freezeGIF_3', {
+        trigger: 'false'
     });
 
-    $('.freezeGIF').click(function(){
+    $(document).ready(function () {
 
-        $(this).stop();
+        card_1.stop(); // stop animation 1
+        card_2.stop(); // stop animation 2
+        card_3.stop(); // stop animation 3
 
     });
 
+    $('.freezeGIF').click(function () {
 
+        data_val = $(this).attr('data-val');
+
+        switch (data_val) {
+
+            case 'card-1':
+                card_1.toggle();
+                card_2.stop();
+                card_3.stop();
+                break;
+
+            case 'card-2':
+                card_2.toggle();
+                card_1.stop();
+                card_3.stop();
+                break;
+
+            case 'card-3':
+                card_3.toggle();
+                card_2.stop();
+                card_1.stop();
+                break;
+
+        }
+
+    });
 
 })(jQuery);
--- template/footer.php
+++ template/footer.php
@@ -1,8 +1,9 @@
 <script src="js/vendor/modernizr-3.11.2.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-  <script type="text/javascript" src="https://unpkg.com/freezeframe@3.0.10/build/js/freezeframe.pkgd.min.js"></script>
+    <script src="./js/vendor/freezeframe.js/packages/freezeframe/dist/freezeframe.min.js"></script>
   <script src="js/plugins.js"></script>
+  <script src="js/multi-step.js"></script>
   <script src="js/main.js"></script>
 
  
