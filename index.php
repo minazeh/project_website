39f236d32c652cafd15e295fe48222a8541bd56e
minazeh <conradng07@gmail.com>
2021-02-04 15:45:00 +0800

    updates for toolart

--- index.php
+++ index.php
@@ -1,21 +1,49 @@
 <?php require './template/header.php'; ?>
 
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand m-2" href="#">
+            <img src="img/toolart-logo.png" alt="" width="217" height="44" class="d-inline-block align-top">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+
+                </li>
+            </ul>
+            <ul class="navbar-nav mb-2 mr-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="https://www.toolart.sg/product-category/product/">SHOP</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://toolart.sg/contact-us/">CONTACT</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://toolart.sg/workshops/">WORKSHOP</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
 <div class="intro-overlay">
 
     <div class="logo-wrapper">
 
-        <img src="img/toolbox-logo.jpg" alt="">
+        <img src="img/fortune-cookie.png" alt="">
 
     </div>
 
     <div class="text-wrapper">
 
         <p>
-            SEND AN AUSPICIOUS<br>CHINESE NEW YEAR COUPLET<br>
-            <small>Customise your own festive greeting and send it to your love ones</small>
+            Send A Personalised Auspicious Greeting & A Fortune Cookie!
         </p>
 
-        <a href="#" class="custom-button get-started">GET STARTED</a>
+        <a href="#" class="custom-button get-started">LET’S GET CRACKING</a>
 
     </div>
 
@@ -27,19 +55,14 @@
     <div class="row justify-content-center">
         <div class="col-12 col-sm-9 col-md-12 col-lg-9 col-xl-9 text-center p-0 mt-3 mb-2">
             <div class="card px-0 pt-4 pb-0 mt-3 mb-3">
-                <div class="main-logo-wrapper">
-
-                    <img src="img/toolbox-logo.jpg" alt="">
-
-                </div>
                 <!-- <p>Customise your own festive greeting card and send it your loved ones!</p> -->
                 <form id="msform">
                     <!-- progressbar -->
                     <ul id="progressbar">
-                        <li class="active" id="account"><strong>Select A Couplet</strong></li>
-                        <li id="personal"><strong>Customise An Auspicious Message</strong></li>
-                        <li id="payment"><strong>Send Your Blessings</strong></li>
-                        <li id="confirm"><strong>Huat Ah!</strong></li>
+                        <li class="active" id="account"><strong>Pick A<br>Fortune Cookie</strong></li>
+                        <li id="personal"><strong>Customise Your Greeting</strong></li>
+                        <li id="payment"><strong>Receiver & Sender Details</strong></li>
+                        <li id="confirm"><strong>Share Your Fortune</strong></li>
                     </ul>
 
                     <fieldset>
@@ -56,13 +79,13 @@
                                             <img src="template/ecard-template/ecard-template-1.png" class="back" alt="">
                                         </div>
                                     </li>
-                                    <li  class="flip-item">
+                                    <li class="flip-item">
                                         <div class="flip-holder">
                                             <img class="gif-item freezeGIF front" data-val="card-2" data-status="false" src="./img/card-template-2.png" alt="">
                                             <img src="template/ecard-template/ecard-template-2.png" class="back" alt="">
                                         </div>
                                     </li>
-                                    <li  class="flip-item">
+                                    <li class="flip-item">
                                         <div class="flip-holder">
                                             <img class="gif-item freezeGIF front" data-val="card-3" data-status="false" src="./img/card-template-3.png" alt="">
                                             <img src="template/ecard-template/ecard-template-3.png" class="back" alt="">
@@ -72,19 +95,19 @@
 
                                 <ul class="gif-selection">
 
-                                    <li  class="flip-item">
+                                    <li class="flip-item">
                                         <div class="flip-holder">
                                             <img class="gif-item freezeGIF front" data-val="card-4" data-status="false" src="./img/card-template-4.png" alt="">
                                             <img src="template/ecard-template/ecard-template-4.png" class="back" alt="">
                                         </div>
                                     </li>
-                                    <li  class="flip-item">
+                                    <li class="flip-item">
                                         <div class="flip-holder">
                                             <img class="gif-item freezeGIF front" data-val="card-5" data-status="false" src="./img/card-template-5.png" alt="">
                                             <img src="template/ecard-template/ecard-template-5.png" class="back" alt="">
                                         </div>
                                     </li>
-                                    
+
                                 </ul>
 
                             </div>
@@ -101,7 +124,7 @@
                                 <div class="col-7">
                                     <h2 class="fs-title">Customise AN AUSPICIOUS Message</h2>
                                 </div>
-                                
+
                             </div>
                             <div class="row">
 
@@ -151,7 +174,7 @@
                                 <div class="col-7">
                                     <h2 class="fs-title text-center">sender and recipient DETAILS</h2>
                                 </div>
-                                
+
                             </div>
 
                             <div class="row justify-content-center">
@@ -161,7 +184,7 @@
                                     <img src="" id="temp_prev" alt="">
 
                                 </div>
-                                
+
                                 <div class="col-6 rec_field">
                                     <label class="fieldlabels">Salutation</label>
                                     <select name="salutation" class="form-control" id="salutation">
@@ -193,7 +216,7 @@
                             </div>
                             <br><br>
                             <div class="row justify-content-center">
-                                <div class="col-12 share-icons"> 
+                                <div class="col-12 share-icons">
                                     <ul>
                                         <li>
                                             <a href="#"><i class="fab fa-whatsapp"></i></a>
